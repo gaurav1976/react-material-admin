570b5db5ef6f283a5080058b4f150d6ad91776fa
Your Name <gaurav.webyug@gmail.com>
2026-01-07 17:24:25 +0530

    commit

--- config-overrides.js
+++ config-overrides.js
@@ -1,31 +1,23 @@
 const webpack = require('webpack');
-const path = require('path');
+
 module.exports = function override(config) {
-  const fallback = config.resolve.fallback || {};
-  Object.assign(fallback, {
+  // ---- Node polyfills ----
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
     crypto: require.resolve('crypto-browserify'),
     stream: require.resolve('stream-browserify'),
-    assert: require.resolve('assert'),
-    http: require.resolve('stream-http'),
-    https: require.resolve('https-browserify'),
-    os: require.resolve('os-browserify'),
-    url: require.resolve('url'),
-    vm: require.resolve('vm-browserify'),
-  });
-  config.resolve.fallback = fallback;
-  config.plugins = (config.plugins || []).concat([
+    buffer: require.resolve('buffer'),
+    process: require.resolve('process/browser'),
+    vm: require.resolve('vm-browserify')
+  };
+
+  config.plugins.push(
     new webpack.ProvidePlugin({
       process: 'process/browser',
-      Buffer: ['buffer', 'Buffer'],
-    }),
-  ]);
-  const modules = config.resolve.modules;
-  config.resolve.modules = [...modules, path.resolve(__dirname, 'src')];
-  config.module.rules.push({
-    test: /\.m?js/,
-    resolve: {
-      fullySpecified: false,
-    },
-  });
+      Buffer: ['buffer', 'Buffer']
+    })
+  );
+
   return config;
 };
+
